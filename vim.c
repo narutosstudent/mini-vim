482e95272a87464ee73059c7be806245bedd3fde
Tiger Abrodi <tigerabrodi@gmail.com>
2023-11-09 06:56:17 +0100

    commit handle arrows

--- vim.c
+++ vim.c
@@ -3,6 +3,11 @@
 #include <stdbool.h>
 #include <string.h>
 
+const char *ARROW_UP = "\033[A";
+const char *ARROW_DOWN = "\033[B";
+const char *ARROW_RIGHT = "\033[C";
+const char *ARROW_LEFT = "\033[D";
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -24,7 +29,8 @@ int main(int argc, char **argv)
 
 	// Start with an initial size and grow as needed
 	size_t size = 1024;
-	size_t used = 0;
+	size_t usedSize = 0;
+	size_t lineCount = 0;
 	char *allCharacters = malloc(size);
 
 	if (allCharacters == NULL)
@@ -38,7 +44,7 @@ int main(int argc, char **argv)
 	while ((ch = fgetc(file)) != EOF)
 	{
 		// Resize the buffer if necessary
-		if (used == size)
+		if (usedSize == size)
 		{
 			// Double the size
 			size *= 2;
@@ -54,7 +60,7 @@ int main(int argc, char **argv)
 		}
 
 		// Here increment happens in next iteration, this is like `allCharacters[used++] = ch`
-		allCharacters[used++] = ch;
+		allCharacters[usedSize++] = ch;
 
 		// increment position of x for every character
 		cursorPositonX++;
@@ -62,13 +68,14 @@ int main(int argc, char **argv)
 		bool isNewLine = ch == '\n';
 		if (isNewLine)
 		{
+			lineCount++;
 			cursorPositonY++;
 			cursorPositonX = 0;
 		}
 	}
 
 	// Add a null terminator to make it a proper C string
-	if (used == size)
+	if (usedSize == size)
 	{
 		// newBuffer to make sure realloc was successful
 		char *newBuffer = realloc(allCharacters, size + 1);
@@ -81,7 +88,7 @@ int main(int argc, char **argv)
 		}
 		allCharacters = newBuffer;
 	}
-	allCharacters[used] = '\0';
+	allCharacters[usedSize] = '\0';
 
 	// Use the string as needed
 	printf("File content:\n%s", allCharacters);
@@ -94,12 +101,60 @@ int main(int argc, char **argv)
 
 	while (1)
 	{
-		char c = getchar();
-		if (c == 'q')
+		// now we have to read multiple characters and also recognize arrow keys, up, down, left and right, we can not only read one character
+
+		// we also have to prevent cursor from going out of bounds (with our cursor position x and y), we have to keep track of cursor position
+
+		char *input = malloc(10);
+		fgets(input, 3, stdin);
+
+		if (strcmp(input, "q\n") == 0)
 		{
 			break;
 		}
-		usleep(1000);
+		else if (strcmp(input, "q\n") == 0)
+		{
+		}
+		else if (strcmp(input, ARROW_UP) == 0)
+		{
+			if (cursorPositonY == 0)
+			{
+				continue;
+			}
+			cursorPositonY--;
+		}
+		else if (strcmp(input, ARROW_DOWN) == 0)
+		{
+			if (cursorPositonY == lineCount)
+			{
+				continue;
+			}
+			cursorPositonY++;
+		}
+		else if (strcmp(input, ARROW_LEFT) == 0)
+		{
+			if (cursorPositonX == 0)
+			{
+				continue;
+			}
+
+			cursorPositonX--;
+		}
+		else if (strcmp(input, ARROW_RIGHT) == 0)
+		{
+			if (cursorPositonX == strlen(allCharacters))
+			{
+				continue;
+			}
+			cursorPositonX++;
+		}
+		else
+		{
+			perror("Error reading input");
+			free(allCharacters);
+			fclose(file);
+			return 1;
+		}
 	}
 
 	// Cleanup
