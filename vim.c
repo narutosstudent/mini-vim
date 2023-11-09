b3477e3b0dec114c61d253139bee7cc9122bb6c0
Tiger Abrodi <tigerabrodi@gmail.com>
2023-11-09 05:24:38 +0100

    correct it

--- vim.c
+++ vim.c
@@ -5,26 +5,77 @@
 
 int main(int argc, char **argv)
 {
-	bool hasTwoArguments = argc == 2;
-	char *allCharacters = malloc(sizeof(char) * 1000);
-
-	if (hasTwoArguments)
+	if (argc != 2)
 	{
-		char *fileName = argv[1];
-		FILE *file = fopen(fileName, "r");
-		if (file == NULL)
-		{
-			perror("Error: File not found");
-			return 1;
-		}
-
-		allCharacters = fgetc(file);
-	}
-	else
-	{
-		perror("Error: Invalid number of arguments");
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
 		return 1;
 	}
+
+	const char *fileName = argv[1];
+	FILE *file = fopen(fileName, "r");
+	if (file == NULL)
+	{
+		perror("Error opening file");
+		return 1;
+	}
+
+	// Start with an initial size and grow as needed
+	size_t size = 1024;
+	size_t used = 0;
+	char *allCharacters = malloc(size);
+
+	if (allCharacters == NULL)
+	{
+		perror("Error allocating memory");
+		fclose(file);
+		return 1;
+	}
+
+	int ch;
+	while ((ch = fgetc(file)) != EOF)
+	{
+		// Resize the buffer if necessary
+		if (used == size)
+		{
+			// Double the size
+			size *= 2;
+			char *newBuffer = realloc(allCharacters, size);
+			if (newBuffer == NULL)
+			{
+				perror("Error reallocating memory");
+				free(allCharacters);
+				fclose(file);
+				return 1;
+			}
+			allCharacters = newBuffer;
+		}
+
+		// Here increment happens in next iteration, this is like `allCharacters[used++] = ch`
+		allCharacters[used++] = ch;
+	}
+
+	// Add a null terminator to make it a proper C string
+	if (used == size)
+	{
+		// newBuffer to make sure realloc was successful
+		char *newBuffer = realloc(allCharacters, size + 1);
+		if (newBuffer == NULL)
+		{
+			perror("Error reallocating memory for null terminator");
+			free(allCharacters);
+			fclose(file);
+			return 1;
+		}
+		allCharacters = newBuffer;
+	}
+	allCharacters[used] = '\0';
+
+	// Use the string as needed
+	printf("File content:\n%s", allCharacters);
+
+	// Cleanup
+	free(allCharacters);
+	fclose(file);
 
 	return 0;
 }
